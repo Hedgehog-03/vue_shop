df3be62f13b993b1f9bb0d729b951f7bcc8c4a42
Hedgehog-03 <1351505656@qq.com>
2021-02-19 17:09:22 +0800

    完成了登录退出功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
+//按需导入按钮
 import { Button } from 'element-ui'
+//按需导入表单
+import { Form, FormItem } from 'element-ui'
+import { Input } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
